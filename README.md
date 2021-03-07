ce81efdbec52c7ec8128544e807e7d0cf3866c3b
Nemo Xiong <xiongnemo@126.com>
2021-03-07 15:29:39 +0800

    feat: multi-monitor support

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Wallpaper shuffler for Windows, implemented in Python.
 
 ## Feature
 
-Shuffle backgound images according to time. Use sets of image provided in dedicated path.
+* Shuffle backgound images according to time. Use sets of image provided in dedicated path.
+
+* Supports multi-monitor.
 
 ```reconstructedText
 nemo@MARSHMALLOW-LAP > D:\temp\image_shuffler_test_dir
@@ -29,9 +31,28 @@ Create a dedicate folder for all your images.
 
 Then, create five subfolders and copy your images into it.
 
+### Multi-monitor config
+
+You will need a json config file.
+
+```json
+{
+    "image_base_directory": "your image's base directory",
+    "per_monitor_directory": {
+        "monitor_0": "override above setting for per monitor",
+        "monitor_1": "...",
+        ...
+    },
+    "dll_path": "dll path to IDesktopWallpaper::SetWallpaper's dll"
+    // download from https://github.com/xiongnemo/idesktopwallpaper-dll/releases/tag/1.0.0
+}
+```
+
 ### Run
 
 Slideshow time is an optional argument.
+
+#### Single
 
 ```powershell
 python ./desktop_background_shuffler.py -p "dir_to_image" -t <slideshow time>
@@ -43,7 +64,15 @@ or
 python ./desktop_background_shuffler.py --PATH="dir_to_image" --TIME=<slideshow time>
 ```
 
+#### Multi
+
+```powershell
+python ./desktop_background_shuffler.py -m multi -c "path_to_json_config_file" -t <slideshow time>
+```
+
 ### Example
+
+#### Single
 
 ```powershell
 python ./desktop_background_shuffler.py -p "D:/temp/image_shuffler_test_dir" -t 50
@@ -52,9 +81,25 @@ python ./desktop_background_shuffler.py -p "D:/temp/image_shuffler_test_dir" -t 
 or
 
 ```powershell
-python python ./desktop_background_shuffler.py --PATH="D:/temp/image_shuffler_test_dir" --TIME=50
+python ./desktop_background_shuffler.py --PATH="D:/temp/image_shuffler_test_dir" --TIME=50
 ```
+
+#### Multi
+
+```powershell
+python ./desktop_background_shuffler.py -m multi -c ./work.json -t 20
+```
+
+## Screenshots
+
+
+### Multi-monitor running in single monitor mode
+
+![](./doc/img/multi-monitor-with-sigle-monitor-mode.png)
+### Multi-monitor mode
+
+![](./doc/img/multi-monitor.png)
 
 ## Todo
 
-- [ ] Use config file for auto configuration.
+- [ ] Use config file for auto configuration (time).
--- desktop_background_shuffler.py
+++ desktop_background_shuffler.py
@@ -2,9 +2,11 @@ import os
 import sys
 import platform
 import ctypes
+from ctypes import *
 import getopt
 import random
 import time
+import json
 
 legal_image_suffix = ['png', 'jpg', 'jpeg', 'bmp']
 
@@ -21,10 +23,45 @@ folder_name_for_config = {
     "night": "night",
 }
 
+()
+
+# https://code.activestate.com/recipes/460509/
+
+
+class RECT(ctypes.Structure):
+    _fields_ = [
+        ('left', ctypes.c_ulong),
+        ('top', ctypes.c_ulong),
+        ('right', ctypes.c_ulong),
+        ('bottom', ctypes.c_ulong)
+    ]
+
+    def dump(self):
+        return map(int, (self.left, self.top, self.right, self.bottom))
+
+
+def get_monitors():
+    retval = []
+    CBFUNC = ctypes.WINFUNCTYPE(
+        ctypes.c_int, ctypes.c_ulong, ctypes.c_ulong, ctypes.POINTER(RECT), ctypes.c_double)
+
+    def cb(hMonitor, hdcMonitor, lprcMonitor, dwData):
+        r = lprcMonitor.contents
+        print("cb: %s %s %s %s %s %s %s %s" % (hMonitor, type(hMonitor), hdcMonitor, type(
+            hdcMonitor), lprcMonitor, type(lprcMonitor), dwData, type(dwData)))
+        data = [hMonitor]
+        data.append(r.dump())
+        retval.append(data)
+        return 1
+    cbfunc = CBFUNC(cb)
+    temp = windll.user32.EnumDisplayMonitors(0, 0, cbfunc, 0)
+    return retval
+
 
 def show_help():
-    print('Usage: desktop_background_shuffler.py -p <base_directory> -t <slideshow time>')
-    print('   or: desktop_background_shuffler.py --PATH=<base_directory> --TIME=<slideshow time>')
+    print('Usage: desktop_background_shuffler.py -p <base_directory_path> -t <slideshow_time> -m <work_mode> -c <config_file_path>')
+    print('   or: desktop_background_shuffler.py --PATH=<base_directory_path> --TIME=<slideshow_time> --MODE=<work_mode> --CONFIG=<config_file_path>')
+    print('If working in multi-monitor mode, a config file is needed.')
 
 
 def check_base_folder_availability(path: str) -> bool:
@@ -75,7 +112,7 @@ def change_desktop_background_with_image(image_path: str):
         SPI_SETDESKWALLPAPER, 0, image_path, SPIF_SENDWININICHANGE)
 
 
-def use_image_in_folder_to_set_background(folder_path: str):
+def find_available_image_within_path(folder_path: str) -> str:
     file_list = os.listdir(os.path.normpath(folder_path))
     while True:
         image_name = random.choice(file_list)
@@ -88,21 +125,101 @@ def use_image_in_folder_to_set_background(folder_path: str):
             break
     image_path = os.path.normpath(
         folder_path + "/" + image_file_relative_path)
-    change_desktop_background_with_image(image_path)
+    return image_path
+
+
+def get_image_path_with_current_time_and_image_base_directory(image_base_directory: str) -> str:
+    year, month, day, hour, minute = map(
+        int, time.strftime("%Y %m %d %H %M").split())
+    current_hour = hour
+    current_min = minute
+    current_time = 100 * hour + minute
+    print("Time: " + str(current_time))
+    if current_time < 600:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["night"])
+    elif current_time < 1120:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["forenoon"])
+    elif current_time < 1330:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["noon"])
+    elif current_time < 1700:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["afternoon"])
+    elif current_time < 1930:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["dusk"])
+    else:
+        return find_available_image_within_path(
+            image_base_directory + "/" + folder_name_for_config["night"])
+
+
+def single_monitor_loop(image_base_directory: str, slideshow_time: int):
+    if image_base_directory == "":
+        print("Missing options.")
+        show_help()
+        exit(3)
+
+    if not check_base_folder_availability(image_base_directory):
+        print("Base directory doesn't meet requirements.")
+        exit(4)
+
+    folder_list = os.listdir(image_base_directory)
+    while True:
+        image_path = get_image_path_with_current_time_and_image_base_directory(
+            image_base_directory)
+        change_desktop_background_with_image(image_path)
+        time.sleep(slideshow_time)
+
+
+def multiple_monitor_loop(config_file_path: str, slideshow_time: int):
+    with open(config_file_path, "r", encoding="utf-8") as raw_config:
+        config_dict = json.loads(raw_config.read())
+    lib = cdll.LoadLibrary(config_dict["dll_path"])
+    monitor_count = len(get_monitors())
+    monitor_specified_base_directory = []
+    print(f"monitor count: {monitor_count}")
+    image_base_directory = config_dict["image_base_directory"]
+    if not check_base_folder_availability(image_base_directory):
+        print("Base directory doesn't meet requirements.")
+        exit(4)
+    for monitor_index in range(0, monitor_count):
+        temp = ""
+        try:
+            temp = config_dict["per_monitor_directory"][f"monitor_{monitor_index}"]
+            if not check_base_folder_availability(temp):
+                print(
+                    f"Per monitor's (#{monitor_index}) base directory doesn't meet requirements. Falling back to default.")
+                temp = config_dict["image_base_directory"]
+        except KeyError:
+            print(
+                f"Per monitor's (#{monitor_index}) base directory not found. Falling back to default.")
+            temp = config_dict["image_base_directory"]
+        monitor_specified_base_directory.append(temp)
+
+    while True:
+        for monitor_index in range(0, monitor_count):
+            image_path = get_image_path_with_current_time_and_image_base_directory(
+                monitor_specified_base_directory[monitor_index])
+            lib.SetWallpaper(monitor_index, image_path)
+        time.sleep(slideshow_time)
 
 
 def main(argv):
 
     if platform.system() != "Windows":
-        print("Unfortunately, we only support Windows.")
+        print("Unfortunately, the script is for Windows only.")
         exit(1)
 
     image_base_directory = ""
     slideshow_time = 60
+    working_mode = ""
+    config_file_path = ""
 
     try:
         opts, args = getopt.getopt(
-            argv, "h:p:t:v:", ["help", "PATH=", "TIME="])
+            argv, "h:p:t:m:c:v:", ["help", "PATH=", "TIME=", "MODE=", "CONFIG="])
     except getopt.GetoptError:
         print("getopt failed to get options.")
         show_help()
@@ -116,42 +233,15 @@ def main(argv):
             image_base_directory = arg
         elif opt in ("-t", "--TIME"):
             slideshow_time = int(arg)
-    if image_base_directory == "":
-        print("Missing options.")
-        show_help()
-        exit(3)
+        elif opt in ("-m", "--MODE"):
+            working_mode = arg
+        elif opt in ("-c", "--CONFIG"):
+            config_file_path = arg
 
-    if not check_base_folder_availability(image_base_directory):
-        print("Base directory doesn't meet requirements.")
-        exit(4)
+    if working_mode != "multi":
+        single_monitor_loop(image_base_directory, slideshow_time)
 
-    folder_list = os.listdir(image_base_directory)
-    while True:
-        year, month, day, hour, minute = map(
-            int, time.strftime("%Y %m %d %H %M").split())
-        current_hour = hour
-        current_min = minute
-        current_time = 100 * hour + minute
-        print("Time: " + str(current_time))
-        if current_time < 600:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["night"])
-        elif current_time < 1120:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["forenoon"])
-        elif current_time < 1330:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["noon"])
-        elif current_time < 1700:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["afternoon"])
-        elif current_time < 1930:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["dusk"])
-        else:
-            use_image_in_folder_to_set_background(
-                image_base_directory + "/" + folder_name_for_config["night"])
-        time.sleep(slideshow_time)
+    multiple_monitor_loop(config_file_path, slideshow_time)
 
 
 if __name__ == "__main__":
